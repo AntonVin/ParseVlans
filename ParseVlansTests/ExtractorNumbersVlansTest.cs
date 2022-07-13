e1e92fdbb85a2f501ccf4d89ba81c8fca0df3e6c
Vinogradov Anton <toshavin@mail.ru>
2022-07-13 17:24:41 +0300

    Добавил первые правильные тесты

--- ParseVlansTests/ExtractorNumbersVlansTest.cs
+++ ParseVlansTests/ExtractorNumbersVlansTest.cs
@@ -7,17 +7,21 @@ namespace ParseVlansTests
         public void GetVlans_return200to300()
         {
             string input = "vlan add 200-210";
-            List<string> output = new List<string>() { "200", "201", "202", "203", "204", "205", "206", "207", "208", "209","210" };
-            ExtractorNumbersVlans extractorNumbersVlans = new ExtractorNumbersVlans(input);
-            Assert.Equal(extractorNumbersVlans.NumbersVlans, output);
+            var exeptedList = new List<string>() { "200", "201", "202", "203", "204", "205", "206", "207", "208", "209","210" };
+
+            List<string> resultList = ExtractorNumbersVlans.GetNumbersVlans(input);
+
+            Assert.Equal(resultList, exeptedList);
         }
         [Fact]
         public void GetVlans_returnCommaVlans()
         {
             string input = "vlan 205,209,100,300";
-            List<string> output = new List<string>() { "205", "209", "100", "300" };
-            ExtractorNumbersVlans extractorNumbersVlans = new ExtractorNumbersVlans(input);
-            Assert.Equal(extractorNumbersVlans.NumbersVlans, output);
+            var exeptedList = new List<string>() { "205", "209", "100", "300" };
+
+            List<string> resultList = ExtractorNumbersVlans.GetNumbersVlans(input);
+
+            Assert.Equal(resultList, exeptedList);
         }
     }
 }
